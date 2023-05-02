60b969cbb7f54ae5877adf9fd782c9fc6794fc9c
hinto.janai <hinto.janai@protonmail.com>
2023-05-01 21:25:49 -0400

    json: change indentation

--- src/json.rs
+++ src/json.rs
@@ -2,13 +2,26 @@
 use anyhow::{anyhow,bail,ensure};
 use std::path::PathBuf;
 use crate::common;
+use serde_json::ser::{Serializer,PrettyFormatter};
 
 //---------------------------------------------------------------------------------------------------- Json
+lazy_static::lazy_static! {
+	pub static ref ENCODING_OPTIONS: PrettyFormatter<'static> = PrettyFormatter::with_indent(b"    ");
+}
+
 crate::common::impl_macro!(Json, "json");
+//crate::common::impl_macro_outer!(Json, "json");
 
 /// [`JSON`](https://docs.rs/serde_json) file format
 ///
 /// File extension is `.json`.
+///
+/// ## Encoding
+/// The encoding option used is:
+/// ```txt
+/// // This is 4 spaces instead of the default 2.
+/// serde_json::ser::PrettyFormatter::with_indent(b"    ");
+/// ```
 ///
 /// ## Safety
 /// When manually implementing, you are **promising** that the `PATH`'s manually specified are correct.
@@ -16,27 +29,32 @@ pub unsafe trait Json: serde::Serialize + serde::de::DeserializeOwned {
 	// Required functions for generic-ness.
 	#[inline(always)]
 	/// Convert [`Self`] to bytes.
-	///
-	/// This uses [`serde_json::ser::to_vec_pretty`];
 	fn to_bytes(&self) -> Result<Vec<u8>, anyhow::Error> {
-		Ok(serde_json::ser::to_vec_pretty(self)?)
+		let mut vec = Vec::with_capacity(128);
+		let mut ser = Serializer::with_formatter(&mut vec, ENCODING_OPTIONS.clone());
+		self.serialize(&mut ser)?;
+		Ok(vec)
 	}
 	#[inline(always)]
 	/// Create [`Self`] from bytes.
 	fn from_bytes(bytes: &[u8]) -> Result<Self, anyhow::Error> {
-		common::convert_error(serde_json::de::from_slice(bytes))
+		Ok(serde_json::de::from_slice(bytes)?)
 	}
 
 	// JSON operations.
 	#[inline(always)]
 	/// This uses [`serde_json::ser::to_string_pretty`];
 	fn to_string(&self) -> Result<String, anyhow::Error> {
-		common::convert_error(serde_json::ser::to_string_pretty(self))
+		let vec = self.to_bytes()?;
+		// SAFETY
+		// `serde_json` doesn't emit
+		// invalid UTF-8 if vec is successful.
+		unsafe { Ok(String::from_utf8_unchecked(vec)) }
 	}
 	#[inline(always)]
-	/// Convert [`Self`] to a [`String`].
+	/// Convert [`Self`] from a [`String`].
 	fn from_string(string: &str) -> Result<Self, anyhow::Error> {
-		common::convert_error(serde_json::de::from_str(string))
+		Ok(serde_json::de::from_str(string)?)
 	}
 
 	// Common functions.
